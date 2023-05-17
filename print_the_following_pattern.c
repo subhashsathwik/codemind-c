2dad6460c05f8880b62d03ed99efba9aca15f358
SATHWIK <119839550+subhashsathwik@users.noreply.github.com>
2023-05-17 11:05:20 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n;i>=1;i--)     {         for(int j=1;j<=i-1;j++)         {             printf(" ");         }         for(int k=1;k<=n;k++)         {             if(i==1 || i==n || k==1 || k==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,22 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++)
+    for(int i=n;i>=1;i--)
     {
-        for(int j=1;j<=i;j++)
+        for(int j=1;j<=i-1;j++)
         {
-            printf("%d",j);
+            printf(" ");
+        }
+        for(int k=1;k<=n;k++)
+        {
+            if(i==1 || i==n || k==1 || k==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
